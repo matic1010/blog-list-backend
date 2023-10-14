0d130a99cfc0c3ce6afa36361f75f9ecd720e5af
Nils Matic <matic1909@gmail.com>
2023-10-14 11:20:36 +0200

    added solution for exercise 4.11

--- tests/blog_api_test.test.js
+++ tests/blog_api_test.test.js
@@ -51,6 +51,44 @@ test('unique identifier is called "id", not "_id"', async () => {
   expect(blogs.body[0]['_id']?.not.toBeDefined);
 });
 
+test('new blog gets posted successfully', async () => {
+  const blogToAdd = {
+    title: 'This is a blog to be added',
+    author: 'Nils Matic',
+    url: 'www.google.com',
+    likes: 0,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(blogToAdd)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await Blog.find({});
+
+  expect(blogsAtEnd.length).toBe(initialBlogs.length + 1);
+  const titles = blogsAtEnd.map((b) => b.title);
+  expect(titles).toContain(blogToAdd.title);
+});
+
+test('if likes property is missing gets dafaulted to 0', async () => {
+  const blogToAdd = {
+    title: 'This is a blog to be added',
+    author: 'Nils Matic',
+    url: 'www.google.com',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(blogToAdd)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await Blog.find({});
+
+  expect(blogsAtEnd.length).toBe(initialBlogs.length + 1);
+  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
